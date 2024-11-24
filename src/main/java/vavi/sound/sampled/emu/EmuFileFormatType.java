96c918ad393c7aa3e502e1e3e5c1e33887ba6637
Naohide Sano <namachapanda2005@gmail.com>
2024-11-24 18:52:32 +0900

    fix access level finish implementing

--- src/main/java/vavi/sound/sampled/emu/EmuFileFormatType.java
+++ src/main/java/vavi/sound/sampled/emu/EmuFileFormatType.java
@@ -8,6 +8,7 @@ package vavi.sound.sampled.emu;
 
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
+import java.util.Arrays;
 import javax.sound.sampled.AudioFileFormat;
 
 import static java.lang.System.getLogger;
@@ -26,11 +27,13 @@ public class EmuFileFormatType extends AudioFileFormat.Type {
     /**
      * Specifies an emulator audio file.
      */
-    public static final EmuFileFormatType NSF = new EmuFileFormatType("NES", "nsf");
-    public static final EmuFileFormatType SFC = new EmuFileFormatType("SNES", "sfc");
-    public static final EmuFileFormatType GBS = new EmuFileFormatType("Game Boy", "gbs");
-    public static final EmuFileFormatType VGM = new EmuFileFormatType("VGM", "vgm");
-    public static final EmuFileFormatType VGZ = new EmuFileFormatType("compressed VGM", "vgz");
+    public static final EmuFileFormatType NSF = new EmuFileFormatType("NES", "nsf", false);
+    public static final EmuFileFormatType SFC = new EmuFileFormatType("SFC", "sfc", false);
+    public static final EmuFileFormatType GBS = new EmuFileFormatType("GBS", "gbs", false);
+    public static final EmuFileFormatType VGM = new EmuFileFormatType("VGM", "vgm", false);
+    public static final EmuFileFormatType VGZ = new EmuFileFormatType("VGM", "vgz", true);
+
+    private final boolean compressed;
 
     /**
      * Constructs a file type.
@@ -38,14 +41,15 @@ public class EmuFileFormatType extends AudioFileFormat.Type {
      * @param name      the name of the emulator audio File Format.
      * @param extension the file extension for this emulator audio File Format.
      */
-    public EmuFileFormatType(String name, String extension) {
+    private EmuFileFormatType(String name, String extension, boolean compressed) {
         super(name, extension);
+        this.compressed = compressed;
     }
 
     private static final EmuFileFormatType[] types = {NSF, SFC, GBS, VGM, VGZ};
 
     public static EmuFileFormatType valueOf(String name, boolean isCompressed) {
 logger.log(Level.TRACE, "name: " + name + ", isCompressed: " + isCompressed);
-        return null; // TODO impl
+        return Arrays.stream(types).filter(t -> name.equalsIgnoreCase(t.toString()) && t.compressed == isCompressed).findFirst().get();
     }
 }
