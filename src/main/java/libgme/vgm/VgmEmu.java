ebcbbf36b0bdf3e57fdc7e462ad60c4224589d4a
Naohide Sano <namachapanda2005@gmail.com>
2025-02-06 20:26:57 +0900

    prepare to use other fm chips

--- src/main/java/libgme/vgm/VgmEmu.java
+++ src/main/java/libgme/vgm/VgmEmu.java
@@ -18,10 +18,13 @@
 
 package libgme.vgm;
 
+import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 
 import libgme.ClassicEmu;
 import libgme.util.DataReader;
+
+import static java.lang.System.getLogger;
 
 
 /**
@@ -30,6 +33,8 @@ import libgme.util.DataReader;
  * @see "https://www.slack.net/~ant"
  */
 public final class VgmEmu extends ClassicEmu {
+
+    private static final Logger logger = getLogger(VgmEmu.class.getName());
 
     public static final String MAGIC = "Vgm ";
 
@@ -54,21 +59,33 @@ public final class VgmEmu extends ClassicEmu {
         int clockRate = getLE32(data, 0x0c);
         if (clockRate == 0)
             clockRate = 3579545;
+logger.log(Level.DEBUG, "clockRate: %08x".formatted(clockRate));
         psgFactor = (int) ((float) psgTimeUnit / vgmRate * clockRate + 0.5);
+        if ((clockRate & 0x4000_0000) != 0) {
+logger.log(Level.DEBUG, "dual apu");
+            apu[1] = new SmsApu();
+        }
 
         // FM clock rate
         fm_clock_rate = getLE32(data, 0x2c);
-        fm = null;
+        fm[0] = null;
         if (fm_clock_rate != 0) {
-            fm = new YM2612();
+            fm_clock_rate &= ~0xc000_0000;
+            fm[0] = new YM2612();
             buf.setVolume(0.7);
-            fm.init(fm_clock_rate, sampleRate());
+            fm[0].init(fm_clock_rate, sampleRate());
+            if ((fm_clock_rate & 0x4000_0000) != 0) {
+logger.log(Level.DEBUG, "dual fm: %08x".formatted(fm_clock_rate));
+                fm[1] = new YM2612();
+                buf.setVolume(0.7);
+                fm[1].init(fm_clock_rate, sampleRate());
+            }
         } else {
             buf.setVolume(1.0);
         }
 
         setClockRate(clockRate);
-        apu.setOutput(buf.center(), buf.left(), buf.right());
+        apu[0].setOutput(buf.center(), buf.left(), buf.right());
 
         return 1;
     }
@@ -94,8 +111,8 @@ public final class VgmEmu extends ClassicEmu {
     static final int psgTimeBits = 12;
     static final int psgTimeUnit = 1 << psgTimeBits;
 
-    final SmsApu apu = new SmsApu();
-    YM2612 fm;
+    final SmsApu[] apu = {new SmsApu(), null};
+    YM2612[] fm = new YM2612[2];
     int fm_clock_rate;
     int pos;
     byte[] data;
@@ -103,7 +120,7 @@ public final class VgmEmu extends ClassicEmu {
     int psgFactor;
     int loopBegin;
     final int[] fm_buf_lr = new int[48000 / 10 * 2];
-    int fm_pos;
+    int[] fm_pos = new int[2];
     int dac_disabled; // -1 if disabled
     int pcm_data;
     int pcm_pos;
@@ -111,18 +128,53 @@ public final class VgmEmu extends ClassicEmu {
 
     static final int cmd_gg_stereo = 0x4F;
     static final int cmd_psg = 0x50;
+    static final int cmd_ym2413 = 0x51;
     static final int cmd_ym2612_port0 = 0x52;
     static final int cmd_ym2612_port1 = 0x53;
+    static final int cmd_ym2151 = 0x54;
     static final int cmd_delay = 0x61;
     static final int cmd_delay_735 = 0x62;
     static final int cmd_delay_882 = 0x63;
+    static final int cmd_byte_delay = 0x64;
     static final int cmd_end = 0x66;
     static final int cmd_data_block = 0x67;
     static final int cmd_short_delay = 0x70;
     static final int cmd_pcm_delay = 0x80;
     static final int cmd_pcm_seek = 0xe0;
+
+    static final int cmd_gg_stereo_2 = 0x3f;
+    static final int cmd_psg_2 = 0x30;
+    static final int cmd_ym2413_2 = 0xa1;
+    static final int cmd_ym2612_2_port0 = 0xa2;
+    static final int cmd_ym2612_2_port1 = 0xa3;
+
     static final int ym2612_dac_port = 0x2a;
     static final int pcm_block_type = 0x00;
+
+    /** */
+    private static int commandLength(int command) {
+        switch (command >> 4) {
+            case 0x03:
+            case 0x04:
+                return 2;
+
+            case 0x05:
+            case 0x0A:
+            case 0x0B:
+                return 3;
+
+            case 0x0C:
+            case 0x0D:
+                return 4;
+
+            case 0x0E:
+            case 0x0F:
+                return 5;
+        }
+
+//        assert false : "unknown %02x".formatted(command);
+        return 1;
+    }
 
     @Override
     public void startTrack(int track) {
@@ -134,9 +186,11 @@ public final class VgmEmu extends ClassicEmu {
         pcm_pos = pos;
         dac_amp = -1;
 
-        apu.reset();
-        if (fm != null)
-            fm.reset();
+        apu[0].reset();
+        if (fm[0] != null)
+            fm[0].reset();
+        if (fm[1] != null)
+            fm[1].reset();
     }
 
     private int toPSGTime(int vgmTime) {
@@ -147,11 +201,11 @@ public final class VgmEmu extends ClassicEmu {
         return countSamples(toPSGTime(vgmTime));
     }
 
-    private void runFM(int vgmTime) {
-        int count = toFMTime(vgmTime) - fm_pos;
+    private void runFM(int index, int vgmTime) {
+        int count = toFMTime(vgmTime) - fm_pos[index];
         if (count > 0) {
-            fm.update(fm_buf_lr, fm_pos, count);
-            fm_pos += count;
+            fm[index].update(fm_buf_lr, fm_pos[index], count);
+            fm_pos[index] += count;
         }
     }
 
@@ -174,7 +228,7 @@ public final class VgmEmu extends ClassicEmu {
             int sampleCount = toFMTime(duration);
             java.util.Arrays.fill(fm_buf_lr, 0, sampleCount * 2, 0);
         }
-        fm_pos = 0;
+        fm_pos[0] = 0;
 
         int time = delay;
         boolean endOfStream = false;
@@ -198,15 +252,44 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
                     break;
 
                 case cmd_gg_stereo:
-                    apu.writeGG(toPSGTime(time), data[pos++] & 0xff);
+                    apu[0].writeGG(toPSGTime(time), data[pos++] & 0xff);
                     break;
 
                 case cmd_psg:
-                    apu.writeData(toPSGTime(time), data[pos++] & 0xff);
+                    apu[0].writeData(toPSGTime(time), data[pos++] & 0xff);
+                    break;
+
+                case cmd_gg_stereo_2:
+                    apu[1].writeGG(toPSGTime(time), data[pos++] & 0xff);
+                    break;
+
+                case cmd_psg_2:
+                    apu[1].writeData(toPSGTime(time), data[pos++] & 0xff);
+                    break;
+
+                case cmd_delay:
+                    time += (data[pos + 1] & 0xff) * 0x100 + (data[pos] & 0xff);
+                    pos += 2;
+                    break;
+
+                case cmd_byte_delay:
+                    time += data[pos++] & 0xff;
+                    break;
+
+                case cmd_ym2413:
+//                    if ( ym2413[0].run_until( to_fm_time( vgm_time ) ) )
+//                        ym2413[0].write( pos [0], pos [1] );
+                    pos += 2;
+                    break;
+
+                case cmd_ym2413_2:
+//                    if ( ym2413[1].run_until( to_fm_time( vgm_time ) ) )
+//                        ym2413[1].write( pos [0], pos [1] );
+                    pos += 2;
                     break;
 
                 case cmd_ym2612_port0:
-                    if (fm != null) {
+                    if (fm[0] != null) {
                         int port = data[pos++] & 0xff;
                         int val = data[pos++] & 0xff;
                         if (port == ym2612_dac_port) {
@@ -216,8 +299,8 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
                                 dac_disabled = (val >> 7 & 1) - 1;
                                 dac_amp |= dac_disabled;
                             }
-                            runFM(time);
-                            fm.write0(port, val);
+                            runFM(0, time);
+                            fm[0].write0(port, val);
                         }
                     } else {
                         pos += 2;
@@ -225,18 +308,42 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
                     break;
 
                 case cmd_ym2612_port1:
-                    if (fm != null) {
-                        runFM(time);
+                    if (fm[0] != null) {
+                        runFM(0, time);
                         int port = data[pos++] & 0xff;
-                        fm.write1(port, data[pos++] & 0xff);
+                        fm[0].write1(port, data[pos++] & 0xff);
                     } else {
                         pos += 2;
                     }
                     break;
 
-                case cmd_delay:
-                    time += (data[pos + 1] & 0xff) * 0x100 + (data[pos] & 0xff);
-                    pos += 2;
+                case cmd_ym2612_2_port0:
+                    if (fm[1] != null) {
+                        int port = data[pos++] & 0xff;
+                        int val = data[pos++] & 0xff;
+                        if (port == ym2612_dac_port) {
+                            write_pcm(time, val);
+                        } else {
+                            if (port == 0x2B) {
+                                dac_disabled = (val >> 7 & 1) - 1;
+                                dac_amp |= dac_disabled;
+                            }
+                            runFM(1, time);
+                            fm[1].write0(port, val);
+                        }
+                    } else {
+                        pos += 2;
+                    }
+                    break;
+
+                case cmd_ym2612_2_port1:
+                    if (fm[1] != null) {
+                        runFM(1, time);
+                        int port = data[pos++] & 0xff;
+                        fm[1].write1(port, data[pos++] & 0xff);
+                    } else {
+                        pos += 2;
+                    }
                     break;
 
                 case cmd_data_block:
@@ -273,19 +380,20 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
                             break;
 
                         default:
-                            setTrackEnded();
-                            logger.log(Level.ERROR, String.format("emulation error: %02x", cmd));
+//                            setTrackEnded();
+                            pos += commandLength(cmd) - 1;
+                            logger.log(Level.WARNING, String.format("emulation error: p: %02x, c: %02x", pos, cmd));
                             break;
                     }
             }
         }
 
-        if (fm != null)
-            runFM(duration);
+        if (fm[0] != null)
+            runFM(0, duration);
 
         int endTime = toPSGTime(duration);
         delay = time - duration;
-        apu.endFrame(endTime);
+        apu[0].endFrame(endTime);
         if (pos >= data.length || endOfStream) {
             setTrackEnded();
             if (pos > data.length) {
@@ -295,18 +403,18 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
             }
         }
 
-        fm_pos = 0;
+        fm_pos[0] = 0;
 
         return endTime;
     }
 
     @Override
     protected void mixSamples(byte[] out, int out_off, int count) {
-        if (fm == null)
+        if (fm[0] == null)
             return;
 
         out_off *= 2;
-        int in_off = fm_pos;
+        int in_off = fm_pos[0];
 
         while (--count >= 0) {
             int s = (out[out_off] << 8) + (out[out_off + 1] & 0xff);
@@ -320,6 +428,6 @@ logger.log(Level.TRACE, "LOOP: " + endlessLoopFlag);
             out_off++;
         }
 
-        fm_pos = in_off;
+        fm_pos[0] = in_off;
     }
 }
